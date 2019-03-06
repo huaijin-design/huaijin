8b038ad0ef17ced036329b94ae3e82950e068f12
chenyueban <chenyueban@qq.com>
2019-03-06 23:23:50 +0800

    test(jest.config.js): delete unused attr

--- jest.config.js
+++ jest.config.js
@@ -9,29 +9,12 @@ module.exports = {
     '/node_modules/',
     '/src/index.ts',
   ],
-  collectCoverageFrom: [
-    'src/components/**/*.{ts,tsx}',
-    'src/components/*/__tests__/**/type.tsx',
-    'src/components/**/*/interface.{ts,tsx}',
-  ],
   moduleFileExtensions: [
     'ts',
     'tsx',
     'js',
     'jsx',
     'json',
-  ],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/es/',
-    '<rootDir>/lib/',
-  ],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/es/',
-    '<rootDir>/lib/',
   ],
   snapshotSerializers: ['jest-emotion']
 };
