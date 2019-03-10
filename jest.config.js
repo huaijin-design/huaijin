3eea7d6e245e1c3b414ffd6f756077c18a6f0005
chenyueban <chenyueban@qq.com>
2019-03-10 22:27:31 +0800

    test(jest.config.js): add testMatch

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,12 @@
 module.exports = {
   rootDir: 'src',
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest'
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
   },
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/src/index.ts',
-  ],
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-    'node',
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/index.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)(spec|test).ts?(x)'],
   snapshotSerializers: ['jest-emotion'],
 };
